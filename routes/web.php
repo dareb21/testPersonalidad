68ea103322b65473e528ea61cfe764813da806a2
dareb21 <qtin10036@gmail.com>
2025-08-22 10:11:46 -0600

    cors

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlockAcontroller;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hola desde el server';
 });
 
 
